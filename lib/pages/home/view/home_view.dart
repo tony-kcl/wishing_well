59f3f44aee8951af54c58bdd5791f53db5ff2848
郭奇靂 <chili@guoqilideMacBook-Pro.local>
2023-11-21 23:15:06 +0800

    Basic Home View

--- lib/pages/home/view/home_view.dart
+++ lib/pages/home/view/home_view.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,6 +7,84 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Text(
+                '寫下你的願望',
+                style: TextStyle(
+                  fontSize: 36,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 7,
+            ),
+            Row(
+              children: [
+                const Expanded(
+                  child: SizedBox(),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.yellow,
+                    height: size.height / 3,
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: TextFormField(
+                      autocorrect: false,
+                      maxLines: null,
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                const Expanded(
+                  child: SizedBox(),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 7,
+            ),
+            Row(
+              children: [
+                const Expanded(
+                  flex: 1,
+                  child: SizedBox()
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: InkWell(
+                      onTap: () {
+                        log('onTap');
+                      },
+                      child: Text(
+                        '送出',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  )
+                ),
+                const Expanded(
+                  flex: 1,
+                  child: SizedBox()
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
